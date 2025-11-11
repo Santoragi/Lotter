92c410a70deb671f5bef7e69e56ae40aabad5717
Santoragi <woojj1254577@naver.com>
2025-11-11 12:16:01 +0900

    init project

---
+++ src/main/java/com/example/lotter/LotterApplication.java
@@ -0,0 +1,13 @@
+package com.example.lotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotterApplication.class, args);
+    }
+
+}
