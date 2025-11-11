feb4fca14c01a86dbef432e8ceaf3b9df09380b7
Santoragi <woojj1254577@naver.com>
2025-11-11 16:48:24 +0900

    feat(NumberGenerator): 난수 생성기
    당첨번호와 보너스 번호를 생성하는 기능을 추가
    - 1~45 범위의 중복되지 않는 숫자 6개(당첨번호)를 생성
    - 1~45 범위의 당첨번호와 중복되지 않는 숫자 1개(보너스번호)를 생성

---
+++ src/main/java/com/example/lotter/lotto/NumberGenerator.java
@@ -0,0 +1,29 @@
+package com.example.lotter.lotto;
+
+import java.util.List;
+import java.util.random.RandomGenerator;
+import java.util.stream.Collectors;
+
+public class NumberGenerator {
+
+    RandomGenerator random = RandomGenerator.getDefault();
+
+    public List<Integer> generateLottoNumbers() {
+
+        return random.ints(1, 46)
+                .distinct()
+                .limit(6)
+                .sorted()
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    public Integer generateBonusNumber(List<Integer> numbers) {
+
+        return random.ints(1, 46)
+                .filter(num -> !numbers.contains(num))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("보너스 번호 생성 실패"));
+    }
+
+}
