8bbec588f9d4bcb87df68d1be963452c097cbd07
Santoragi <woojj1254577@naver.com>
2025-11-12 13:44:03 +0900

    refactor: 로또 번호 개수, 범위 상수화

---
+++ src/main/java/com/example/lotter/lotto/constant/LottoConstant.java
@@ -0,0 +1,8 @@
+package com.example.lotter.lotto.constant;
+
+public class LottoConstant {
+    public static final int LOTTO_PRICE = 5000;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+}
--- src/main/java/com/example/lotter/lotto/util/NumberGenerator.java
+++ src/main/java/com/example/lotter/lotto/util/NumberGenerator.java
@@ -1,5 +1,9 @@
 package com.example.lotter.lotto.util;
 
+import static com.example.lotter.lotto.constant.LottoConstant.LOTTO_NUMBER_COUNT;
+import static com.example.lotter.lotto.constant.LottoConstant.MAX_NUMBER;
+import static com.example.lotter.lotto.constant.LottoConstant.MIN_NUMBER;
+
 import java.util.List;
 import java.util.random.RandomGenerator;
 import java.util.stream.Collectors;
@@ -12,9 +16,9 @@ public class NumberGenerator {
 
     public List<Integer> generateLottoNumbers() {
 
-        return random.ints(1, 46)
+        return random.ints(MIN_NUMBER, MAX_NUMBER)
                 .distinct()
-                .limit(6)
+                .limit(LOTTO_NUMBER_COUNT)
                 .sorted()
                 .boxed()
                 .collect(Collectors.toList());
@@ -22,7 +26,7 @@ public class NumberGenerator {
 
     public Integer generateBonusNumber(List<Integer> numbers) {
 
-        return random.ints(1, 46)
+        return random.ints(MIN_NUMBER, MAX_NUMBER)
                 .filter(num -> !numbers.contains(num))
                 .findFirst()
                 .orElseThrow(() -> new IllegalStateException("보너스 번호 생성 실패"));
